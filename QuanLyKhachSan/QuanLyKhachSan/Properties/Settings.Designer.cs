17c596abe8645c8bdf0ba51935b866a92034972d
dau-go <hosyviet95@gmail.com>
2016-11-04 21:53:34 +0700

    đã xong

--- QuanLyKhachSan/QuanLyKhachSan/Properties/Settings.Designer.cs
+++ QuanLyKhachSan/QuanLyKhachSan/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace QuanLyKhachSan.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "12.0.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -20,6 +20,17 @@ namespace QuanLyKhachSan.Properties {
         public static Settings Default {
             get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-6NJFH9A\\SQLEXPRESS;Initial Catalog=QuanLyKhachSan;Integrated " +
+            "Security=True")]
+        public string QuanLyKhachSanConnectionString {
+            get {
+                return ((string)(this["QuanLyKhachSanConnectionString"]));
             }
         }
     }
