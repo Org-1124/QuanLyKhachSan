63c5886036a3ef64bce2a64c02a6d86357b2a484
lehaisonmath6 <lehaisonmath6@gmail.com>
2016-11-04 08:41:21 +0700

    them do dung

--- QuanLyKhachSan/QuanLyKhachSan/Properties/Settings.Designer.cs
+++ QuanLyKhachSan/QuanLyKhachSan/Properties/Settings.Designer.cs
@@ -22,5 +22,16 @@ namespace QuanLyKhachSan.Properties {
                 return defaultInstance;
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.\\SQLEXPRESS;Initial Catalog=QuanLyKhachSan;Integrated Security=True;" +
+            "Encrypt=False;TrustServerCertificate=True")]
+        public string QuanLyKhachSanConnectionString {
+            get {
+                return ((string)(this["QuanLyKhachSanConnectionString"]));
+            }
+        }
     }
 }
